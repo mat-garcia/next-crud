5fe8bfd6c3c05d03940e499cb8fc1e8c5411de7e
mat-garcia <mateussoap@gmail.com>
2022-08-29 22:28:20 -0300

    cfg inicial e primeiros componentes

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-gradient-to-tr from-blue-500 to-purple-500 text-white">
+      <Layout title='Cadastro Simples'>
+        <span> Content </span>
+      </Layout>
+    </div>
+    
+  )
+}
